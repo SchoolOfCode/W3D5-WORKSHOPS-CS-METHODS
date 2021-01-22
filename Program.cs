e2fb6bc5316cca3fccbf401d93e91e9d91b4144b
Ellie <ellieredmayne@hotmail.com>
2021-01-22 10:41:53 +0000

    Task 4 .StartsWith

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace workshops_cs_methods
             int square = Square(16);
             decimal getvat = GetVat(50.5M);
             bool a = StartsWithChar("hat");
+            bool word = StartsWithCharOther ("Zoo");
             Console.WriteLine(a);
+            Console.WriteLine(word);
             Console.WriteLine(sum);
             Console.WriteLine(square);
             Console.WriteLine(getvat);
@@ -38,6 +40,10 @@ namespace workshops_cs_methods
             } else {
                 return false;
             }
+        }
+        static bool StartsWithCharOther (string word)
+        {
+            return word.StartsWith('Z');
         }
     }
 }
